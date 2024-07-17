dc09b51c7b73d1e3b225b806bc6c1579d1f65174
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2024-07-17 10:41:31 +0300

    task completed

--- module_1/src/main/java/com/github/N1ckBaran0v/Main.java
+++ module_1/src/main/java/com/github/N1ckBaran0v/Main.java
@@ -1,11 +1,19 @@
 package com.github.N1ckBaran0v;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.Log;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MirrorServlet servlet = new MirrorServlet();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(servlet), "/mirror");
+        Server server = new Server(8080);
+        server.setHandler(context);
+        server.start();
+        Log.getLog().info("Server started");
+        server.join();
     }
 }
